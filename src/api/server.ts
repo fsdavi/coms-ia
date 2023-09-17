dce5834c76fc29de78857ad7271f129737de453e
Davi Freire <fsdavi.dev@gmail.com>
2023-09-17 18:10:06 -0300

    feat: implements all new routes and cors to our server

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,11 +1,21 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import { getAllPromptsRoute } from "@/api/routes/get-all-prompts";
+import { uploadVideoRoute } from "@/api/routes/upload-video";
+import { createTranscriptionRoute } from "@/api/routes/create-transcription";
+import { generateAICompletionRoute } from "@/api/routes/generate-ai-completion";
 
-const app = fastify()
+const app = fastify();
 
-app.get("/", async () => {
-  return "Hello World";
+app.register(fastifyCors, {
+  origin: "*",
 });
 
-app.listen({ port: 3030}).then(() => {
- console.log('API is running on 3030!')
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(generateAICompletionRoute);
+
+app.listen({ port: 3030 }).then(() => {
+  console.log("API is running on 3030!");
 });
